1660f9215c7d909b643847a4f3e311fe6a7abe01
Zac Hooper <zac.g.hooper@gmail.com>
2020-05-02 09:01:01 +0100

    Completed Part 2

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,43 +1,33 @@
 import React from 'react';
 import './Business.css'
 
-const business = {
-    imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-    name: 'MarginOtto Pizzeria',
-    address: '1010 Paddington Way',
-    city: 'Flavortown',
-    state: 'NY',
-    zipCode: '10101',
-    category: 'Italian',
-    rating: 4.5,
-    reviewCount: 90
-};
-
-/* class Business extends React.Component {
+export class Business extends React.Component {
     render() {
+        console.log(this.props.imageSrc);
         return (
             <div className="Business">
                 <div className="image-container">
-                    <img src={business.imageSrc} alt={business.name}/>
+                    <img src={this.props.business.imageSrc} alt={this.props.business.name}/>
                 </div>
-                <h2>{business.name}</h2>
+                <h2>{this.props.business.name}</h2>
                 <div className="Business-information">
                     <div className="Business-address">
-                        <p>{business.address}</p>
-                        <p>{business.city}</p>
-                        <p>{business.state} {business.zipCode}</p>
+                        <p>{this.props.business.address}</p>
+                        <p>{this.props.business.city}</p>
+                        <p>{this.props.business.state} {this.props.business.zipCode}</p>
                     </div>
                     <div className="Business-reviews">
-                        <h3>{business.category}</h3>
-                        <h3 className="rating">{business.rating} Stars</h3>
-                        <p>{business.reviewCount} Reviews</p>
+                        <h3>{this.props.business.category}</h3>
+                        <h3 className="rating">{this.props.business.rating} Stars</h3>
+                        <p>{this.props.business.reviewCount} Reviews</p>
                     </div>
                 </div>
             </div>
         );
     }
-} */
+} 
 
+/*
 export function Business () {
     return (
         <div className="Business">
@@ -60,5 +50,6 @@ export function Business () {
         </div>
     );
 }
+*/
 
 export default Business;
--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -2,22 +2,22 @@ import React from 'react';
 import './BusinessList.css';
 import { Business } from '../Business/Business';
 
-/*
+
 class BusinessList extends React.Component {
     render() {
         return (
             <div className="BusinessList">
-                <Business />
-                <Business />
-                <Business />
-                <Business />
-                <Business />
-                <Business />
+                {
+                    this.props.businesses.map((business, index) => {
+                        return <Business key={"business_" + index} business={business} />
+                    })
+                }
             </div>
         );
     }
-} */
+}
 
+/*
 function BusinessList() {
     return (
         <div className="BusinessList">
@@ -30,5 +30,6 @@ function BusinessList() {
         </div>
     );
 }
+*/
 
 export default BusinessList;
